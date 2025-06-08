abade6d8e3a3ceed13590fbb6bdb48d7aede9ea3
may_h_yun <95019698+leeseohyun0630@users.noreply.github.com>
2025-06-08 18:58:42 +0900

    [수정] 구조 리팩터링

--- src/main/java/com/example/demo/CertQuest_BackendApplication.java
+++ src/main/java/com/example/demo/CertQuest_BackendApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.example.demo.mapper")
-public class ProjectHnApplication {
+public class CertQuest_BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectHnApplication.class, args);
+		SpringApplication.run(CertQuest_BackendApplication.class, args);
 	}
 
 }
